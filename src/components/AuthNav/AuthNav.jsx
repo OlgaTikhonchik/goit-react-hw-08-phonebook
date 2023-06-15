c575bd779c6537af8f8efb87387dae069c43eca8
OlgaTikhonchik <olga882011@gmail.com>
2023-06-15 18:50:03 +0300

    Create AuthNav.jsx

---
+++ src/components/AuthNav/AuthNav.jsx
@@ -0,0 +1,10 @@
+import { NavLink } from 'react-router-dom';
+
+export const AuthNav = () => {
+  return (
+    <div>
+      <NavLink to="/register">Register</NavLink>
+      <NavLink to="/login">Log In</NavLink>
+    </div>
+  );
+};
