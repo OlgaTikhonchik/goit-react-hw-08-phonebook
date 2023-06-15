9d6295fb04e8d9aa095d1b2d01a212d6ce711137
OlgaTikhonchik <olga882011@gmail.com>
2023-06-15 18:49:08 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,51 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { contactsReducer } from './contactsSlice';
-import { filterReducer } from './filterSlice';
+// import { contactsReducer } from './contactsSlice';
+// import { filterReducer } from './filterSlice';
+
+// export const store = configureStore({
+//   reducer: {
+//     contacts: contactsReducer,
+//     filter: filterReducer,
+//   },
+// });
+
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { authReducer } from './auth/auth.slice';
+import { rootReducer } from './rootReducer';
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
+
+const persistedReducer = persistReducer(authPersistConfig, authReducer);
 
 export const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
-    filter: filterReducer,
+    auth: persistedReducer,
+    contacts: rootReducer,
+    // contacts: contactsReducer,
+    // filter: filterReducer,
   },
+  middleware(getDefaultMiddleware) {
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    });
+  },
+  devTools: process.env.NODE_ENV === 'development',
 });
+
+export const persistor = persistStore(store);
