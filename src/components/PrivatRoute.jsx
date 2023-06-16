697fa1dba588af04fece0f870db3adc010e47545
OlgaTikhonchik <olga882011@gmail.com>
2023-06-16 17:56:05 +0300

    Update PrivatRoute.jsx

--- src/components/PrivatRoute.jsx
+++ src/components/PrivatRoute.jsx
@@ -4,6 +4,8 @@ import { Navigate } from 'react-router-dom';
 export const PrivateRoute = ({ component: Component, redirectTo = '/' }) => {
   const { isLoggedIn, isRefreshing } = useAuth();
   const shouldRedirect = !isLoggedIn && !isRefreshing;
-
+  console.log(isLoggedIn);
+  console.log(isRefreshing);
+  console.log(shouldRedirect);
   return shouldRedirect ? <Navigate to={redirectTo} /> : Component;
 };
