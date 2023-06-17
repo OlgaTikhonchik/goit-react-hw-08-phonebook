0fca7437eba8330e7753ddc47f9571f3f181e8eb
OlgaTikhonchik <olga882011@gmail.com>
2023-06-17 22:33:12 +0300

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,7 +2,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { nanoid } from 'nanoid';
 import { ContainerFilter, Input, Label } from './Filter.styled';
 import { setFilter } from 'redux/filterSlice';
-import { selectFilter } from 'redux/selectors';
+// import { selectFilter } from 'redux/selectors';
 
 export const Filter = () => {
   const dispatch = useDispatch();
