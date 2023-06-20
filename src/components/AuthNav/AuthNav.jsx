03f1618b3dec12975a77e4e2ab6a1988a406e4c1
OlgaTikhonchik <olga882011@gmail.com>
2023-06-20 14:59:22 +0300

    Update AuthNav.jsx

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,10 +1,32 @@
 import { NavLink } from 'react-router-dom';
+import MUI from 'components/MUI';
+import HowToRegIcon from '@mui/icons-material/HowToReg';
+import LoginIcon from '@mui/icons-material/Login';
 
 export const AuthNav = () => {
   return (
-    <div>
-      <NavLink to="/register">Register</NavLink>
-      <NavLink to="/login">Log In</NavLink>
-    </div>
+    // <div>
+    <MUI.Toolbar>
+      <NavLink to="/register">
+        {' '}
+        <MUI.Button
+          startIcon={<HowToRegIcon />}
+          sx={{ color: 'white', margin: '10px' }}
+          variant="outlined"
+        >
+          Register
+        </MUI.Button>
+      </NavLink>
+      <NavLink to="/login">
+        <MUI.Button
+          startIcon={<LoginIcon />}
+          sx={{ color: 'white' }}
+          variant="outlined"
+        >
+          Log In
+        </MUI.Button>
+      </NavLink>
+    </MUI.Toolbar>
+    // </div>
   );
 };
