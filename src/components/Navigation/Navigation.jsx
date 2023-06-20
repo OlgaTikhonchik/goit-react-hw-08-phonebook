da9407c66561404a7b531663be2a7d5f2c74b3cc
OlgaTikhonchik <olga882011@gmail.com>
2023-06-20 14:59:00 +0300

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,13 +1,46 @@
 import { useAuth } from 'hooks/useAuth';
 import { NavLink } from 'react-router-dom';
+import MUI from 'components/MUI';
+import HomeIcon from '@mui/icons-material/Home';
+import RecentActorsIcon from '@mui/icons-material/RecentActors';
 
 export const Navigation = () => {
   const { IsLoggedIn } = useAuth();
   return (
-    <nav>
-      <NavLink to="/">Home</NavLink>
+    // <nav>
 
-      {IsLoggedIn && <NavLink to="/contacts">Contacts</NavLink>}
-    </nav>
+    <MUI.Toolbar>
+      <MUI.Typography
+        variant="h6"
+        noWrap
+        component="div"
+        sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+      >
+        PHONEBOOK
+      </MUI.Typography>
+
+      <NavLink to="/">
+        <MUI.Button
+          startIcon={<HomeIcon />}
+          sx={{ color: 'white', margin: '10px' }}
+          variant="outlined"
+        >
+          Home
+        </MUI.Button>
+      </NavLink>
+
+      {IsLoggedIn && (
+        <NavLink to="/contacts">
+          <MUI.Button
+            startIcon={<RecentActorsIcon />}
+            sx={{ color: 'white' }}
+            variant="outlined"
+          >
+            Contacts
+          </MUI.Button>
+        </NavLink>
+      )}
+    </MUI.Toolbar>
+    /* </nav> */
   );
 };
