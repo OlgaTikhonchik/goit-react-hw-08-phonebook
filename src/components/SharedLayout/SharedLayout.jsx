dfac3468ecfe7959e0e31bacc68addca6068e19f
OlgaTikhonchik <olga882011@gmail.com>
2023-06-20 14:58:25 +0300

    Update SharedLayout.jsx

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,14 +1,19 @@
 import { AppBar } from 'components/AppBar/AppBar';
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
+import MUI from 'components/MUI';
 
 export const SharedLayout = () => {
   return (
-    <div>
+    <>
       <AppBar />
-      <Suspense fallback={<div>Loading....</div>}>
-        <Outlet />
-      </Suspense>
-    </div>
+      <MUI.Container
+        sx={{ marginTop: '20px', textAlign: 'center', color: 'white' }}
+      >
+        <Suspense fallback={<div>Loading....</div>}>
+          <Outlet />
+        </Suspense>
+      </MUI.Container>
+    </>
   );
 };
