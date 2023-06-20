b276a50cadf709b6d4766d4b64ce2aa5e5a23c0e
OlgaTikhonchik <olga882011@gmail.com>
2023-06-20 14:58:32 +0300

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,5 +1,8 @@
 export const selectContacts = state => state.contacts.items;
 export const selectFilter = state => state.filter;
 
-export const selectIsLoading = state => state.contacts.isLoading;
-export const selectError = state => state.contacts.error;
+// export const selectIsLoading = state => state.contacts.isLoading;
+// export const selectError = state => state.contacts.error;
+export const selectIsLoading = state => state.contacts.contacts.isLoading;
+
+export const selectError = state => state.contacts.contacts.error;
