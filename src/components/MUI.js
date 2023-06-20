2093f49a6c804ef988b74a8fba952a260ea900c9
OlgaTikhonchik <olga882011@gmail.com>
2023-06-20 14:59:19 +0300

    Create MUI.js

---
+++ src/components/MUI.js
@@ -0,0 +1,28 @@
+import {
+  AppBar,
+  Container,
+  Typography,
+  Box,
+  Toolbar,
+  Chip,
+  Button,
+  TextField,
+  Icon,
+  Card,
+  CardMedia,
+} from '@mui/material';
+
+const MUI = {
+  AppBar,
+  Container,
+  Typography,
+  Box,
+  Toolbar,
+  Chip,
+  Button,
+  TextField,
+  Icon,
+  Card,
+  CardMedia,
+};
+export default MUI;
