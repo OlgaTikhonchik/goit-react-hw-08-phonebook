b30069badfce8ae291a53eefd182489b1ff345cf
OlgaTikhonchik <olga882011@gmail.com>
2023-06-20 14:59:25 +0300

    Update AppBar.jsx

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -2,14 +2,27 @@ import { AuthNav } from 'components/AuthNav/AuthNav';
 import { Navigation } from 'components/Navigation/Navigation';
 import { UserMenu } from 'components/UserMenu/UserMenu';
 import { useAuth } from 'hooks/useAuth';
+import MUI from 'components/MUI';
 
 export const AppBar = () => {
   const { isLoggedIn } = useAuth();
 
   return (
-    <header>
-      <Navigation />
-      {isLoggedIn ? <UserMenu /> : <AuthNav />}
-    </header>
+    <MUI.AppBar position="static">
+      <header style={{ backgroundColor: '#bba095' }}>
+        <MUI.Container
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            fontWeight: 700,
+            // backgroundColor: 'SaddleBrown',
+            color: '#653463',
+          }}
+        >
+          <Navigation />
+          <MUI.Toolbar>{isLoggedIn ? <UserMenu /> : <AuthNav />}</MUI.Toolbar>
+        </MUI.Container>
+      </header>
+    </MUI.AppBar>
   );
 };
