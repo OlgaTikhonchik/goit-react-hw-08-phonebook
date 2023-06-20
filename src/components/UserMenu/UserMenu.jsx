47702601394215c898e29e2b8589989544e24e48
OlgaTikhonchik <olga882011@gmail.com>
2023-06-20 14:58:57 +0300

    Update UserMenu.jsx

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,16 +1,36 @@
 import { useAuth } from 'hooks/useAuth';
 import { useDispatch } from 'react-redux';
 import { logOut } from 'redux/auth/operationsAuth';
+import MUI from 'components/MUI';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 export const UserMenu = () => {
   const dispatch = useDispatch();
   const { user } = useAuth();
   return (
     <div>
-      <p>Welcome, {user.name}</p>
-      <button type="button" onClick={() => dispatch(logOut())}>
-        LogOut
-      </button>
+      <MUI.Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          gap: '2rem',
+          alignItems: 'center',
+        }}
+      >
+        <MUI.Box>
+          <p>Welcome, {user.name}</p>
+        </MUI.Box>
+        <MUI.Button
+          startIcon={<LogoutIcon />}
+          variant="contained"
+          color="secondary"
+          sx={{ height: 'max-content' }}
+          type="button"
+          onClick={() => dispatch(logOut())}
+        >
+          LogOut
+        </MUI.Button>
+      </MUI.Box>
     </div>
   );
 };
